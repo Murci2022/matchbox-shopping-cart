99b05e371c2b7f664d8de9403cd76e2c4c9eeffe
MK <Javascript@mates-air.localdomain>
2023-08-03 19:37:04 +0200

    add nav

---
+++ src/cartItems.js
@@ -0,0 +1,37 @@
+import BugattiGold from './images/Bugatti Type 57 Atlantic 1938.png';
+import BugattiBlack from './images/Bugatti Type 57 Atlantic 1938:.png.png';
+import Ferrari from './images/Ferrari 250 GTO:.png.png';
+import Porsche from './images/Porsche 911 1963.png';
+
+const cartItems = [
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Bugatti Type 57 Atlantic Gold',
+    price: '298,00',
+    img: BugattiGold,
+    amount: 1,
+  },
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Bugatti Type 57 Atlantic black',
+    price: '298,00',
+    img: BugattiBlack,
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Porsche',
+    price: '315,00',
+    img: Porsche,
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    title: 'Ferrari',
+    price: '250,00',
+    img: Ferrari,
+    amount: 1,
+  },
+];
+
+export default cartItems;
